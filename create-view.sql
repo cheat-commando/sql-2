18243d6ea37da56ad386d3af23273a0260fd5f6f
Carston Work <carston.work@gmail.com>
2021-08-12 15:05:00 -0600

    Finished create-view

--- create-view.sql
+++ create-view.sql
@@ -1,5 +1,15 @@
 -- Create a view called rock that selects all the tracks where the genre is Rock.
 
-
+CREATE VIEW rock AS
+SELECT name
+FROM track
+WHERE genre_id IN (
+  SELECT genre_id FROM genre WHERE name = 'Rock');
 
 -- Create a view called classical_count that gets a count of all the tracks from the playlist called Classical.
+
+CREATE VIEW classical_count AS
+SELECT Count(track_id)
+FROM playlist_track
+WHERE playlist_id IN (
+  SELECT playlist_id FROM playlist WHERE name = 'Classical');
